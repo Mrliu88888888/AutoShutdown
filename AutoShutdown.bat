49f7c3f745c3a8f23271e2b36e4320499974f490
MengLiu <51469101+Mrliu88888888@users.noreply.github.com>
2025-03-14 10:09:46 +0800

    (⌐■_■)

--- AutoShutdown.bat
+++ AutoShutdown.bat
@@ -1,7 +1,6 @@
 @echo off
 chcp 65001
 cls
-
 PowerShell -Command "shutdown /s /t ([math]::Round(((Get-Date -Hour 18 -Minute 0 -Second 0) - (Get-Date)).TotalSeconds))"
 if %ERRORLEVEL% neq 0 (
 	echo.
