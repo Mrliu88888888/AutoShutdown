6ca4e9c3b7954d59979c675b4fe901f8fda516f0
MengLiu <51469101+Mrliu88888888@users.noreply.github.com>
2025-03-14 09:43:21 +0800

    Add files via upload

---
+++ AutoShutdown.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001
+cls
+PowerShell -Command "shutdown /s /t ([math]::Round(((Get-Date -Hour 18 -Minute 0 -Second 0) - (Get-Date)).TotalSeconds))"
+if %ERRORLEVEL% neq 0 (
+	echo.
+	echo ==================================================
+	echo [错误] 自动关机设置失败
+	echo ==================================================
+	echo.
+	pause
+)
